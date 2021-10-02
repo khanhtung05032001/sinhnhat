b631ced0974ca16c1cc14974dfeb294d55d88cca
khanhtung05032001 <sontung130701@gmail.com>
2021-10-02 17:17:37 +0700

    uthgn

--- js/yokattaa.js
+++ js/yokattaa.js
@@ -13,46 +13,11 @@
         $(this).text("tung");
     })
 
-    var ttu =["t","u","n","g"]
-    for (var i=0;i<ttu.length;i++){
-        console.log(i);
-    }
-    document.querySelector(".son").oninput = () =>{
-        document.querySelector(".st").innerHTML =   ttu[1] + ttu[2];
-    }
-
-// $(".son").click(function (){
-//     $(this).innerHTML="utng";
-// })
 
 
 
 
-// const danhsach = document.getElementById("#kk")
-// const con = danhsach.children
 
-// function keydownHandler(evt)  {
-//     appendLog("Keydown!");
-// }
-//
-// function keyupHandler(evt)  {
-//     appendLog("Keyup!");
-// }
 
-// function keypressHandler(evt)  {
-//     appendLog("tung");
-//
-// }
-//
-//
-//
-// function appendLog(text)  {
-//     var oldText = document.getElementById("log-div").value;
-//     document.getElementById("log-div").value = oldText+ text;
-// }
-
-// function clearLog()  {
-//     document.getElementById("log-div").value = "";
-// }
 
 
